df4b78f42f18ad0e14f46b6a46a0d9c8465ae358
nazariyv <nazariy@inbox.ru>
2020-09-21 18:48:50 +0100

    CardInfo example

---
+++ src/stories/CardInfo.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// ! TODO: nado chtobu komponentu nazuvalis' kak i sam faijl. To jest' zamenit' CardDop na CardInfo
+import CardInfo from '../components/LocCards/CardDop';
+
+export default {
+  title: 'Example/CardInfo',
+  component: CardInfo,
+};
+
+const Template = (args) => <CardInfo {...args} />;
+
+export const Default = Template.bind({});
