2dbbefee08d323e1c34a14ed3072c02227ff9988
nazariyv <nazariy@inbox.ru>
2020-09-22 14:32:42 +0100

    published this to npm

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Home from "./views/HomePage/HomePage";
+import Explore from "./views/ExplorerPage";
+import ExploreView from "./views/ExplorerVievPage";
+import Lead from "./views/LeadPage";
+import MySettings from "./views/MySettingPage";
+import MyDashboard from "./views/MyProfilePage/MyProfilePage";
+import CreateBounty from "./views/CreateBount";
+
+export {Home, Explore, ExploreView, Lead, MySettings, MyDashboard, CreateBounty};
